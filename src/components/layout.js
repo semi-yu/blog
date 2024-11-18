acf376eea1272f58a32f65caac27d030db3ce631
armedHeistKino <urban.manul.1919@gmail.com>
2024-11-18 21:59:02 +0900

    fix: Link 문제 해결중

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,8 +8,8 @@ const Layout = ({ children }) => {
             <Breadcrumb />
             <nav>
                 <ul className="no-bullets nav-bar-text">
-                    <li><Link className="link-body-emphasis link-offset-2 link-underline-opacity-0 link-underline-opacity-0-hover" to="/articles">글모음 articles</Link></li>
-                    <li><Link className="link-body-emphasis link-offset-2 link-underline-opacity-0 link-underline-opacity-0-hover" to="/about">대하여 about</Link></li>
+                    <li><Link className="link-body-emphasis link-offset-2 link-underline-opacity-0 link-underline-opacity-0-hover" to="/articles/">글모음 articles</Link></li>
+                    <li><Link className="link-body-emphasis link-offset-2 link-underline-opacity-0 link-underline-opacity-0-hover" to="/about/">대하여 about</Link></li>
                 </ul>
             </nav>
             <main>
